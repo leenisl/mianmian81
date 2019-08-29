4134ff4f41488fbbbd3b275bed88eb307120582c
xuzuoxiao <2252246350@qq.com>
2019-08-29 19:23:37 +0800

    初始化

--- src/module-hmmm/pages/directorys.vue
+++ src/module-hmmm/pages/directorys.vue
@@ -1,8 +1,8 @@
 <template>
   <el-form class="breadcrumb">
     <el-form-item>
-      <el-breadcrumb separator-class="el-icon-arrow-right">
-        <el-breadcrumb-item :to="{ path: '/' }" style="color:pink">学科管理</el-breadcrumb-item>
+      <el-breadcrumb >
+        <el-breadcrumb-item :to="{path:'/'}">学科管理</el-breadcrumb-item>
         <el-breadcrumb-item>javaEE</el-breadcrumb-item>
         <el-breadcrumb-item>目录管理</el-breadcrumb-item>
       </el-breadcrumb>
@@ -11,9 +11,8 @@
     <el-button type="info">新建目录</el-button>
     <el-button type="info">返回学科</el-button>
   </el-form-item>
-  <el-form-item>
-    <span style="">目录</span>
-    <el-input placeholder="请输入"></el-input>
+  <el-form-item label="目录">
+    <el-input placeholder="请输入" style="width:200px"></el-input>
   </el-form-item>
   </el-form>
 </template>
@@ -31,5 +30,11 @@ export default {
 .breadcrumb{
   margin-top: 15px;
   margin-left: 15px;
+}
+.el-breadcrumb__inner a, .el-breadcrumb__inner.is-link{
+  color: #000;
+}
+.el-breadcrumb__inner.is-link{
+  color: black !important;
 }
 </style>
