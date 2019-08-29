554039595769f0154c3a49c87d49b08b8a294570
xuzuoxiao <2252246350@qq.com>
2019-08-29 16:41:59 +0800

    xzx 初始化目录模块

--- src/module-hmmm/pages/directorys.vue
+++ src/module-hmmm/pages/directorys.vue
@@ -1,7 +1,21 @@
 <template>
-  <div class="dashboard-container">
-    <div class="app-container">目录管理</div>
-  </div>
+  <el-form class="breadcrumb">
+    <el-form-item>
+      <el-breadcrumb separator-class="el-icon-arrow-right">
+        <el-breadcrumb-item :to="{ path: '/' }" style="color:pink">学科管理</el-breadcrumb-item>
+        <el-breadcrumb-item>javaEE</el-breadcrumb-item>
+        <el-breadcrumb-item>目录管理</el-breadcrumb-item>
+      </el-breadcrumb>
+    </el-form-item>
+  <el-form-item>
+    <el-button type="info">新建目录</el-button>
+    <el-button type="info">返回学科</el-button>
+  </el-form-item>
+  <el-form-item>
+    <span style="">目录</span>
+    <el-input placeholder="请输入"></el-input>
+  </el-form-item>
+  </el-form>
 </template>
 
 <script>
@@ -13,5 +27,9 @@ export default {
 }
 </script>
 
-<style scoped>
+<style  scoped>
+.breadcrumb{
+  margin-top: 15px;
+  margin-left: 15px;
+}
 </style>
